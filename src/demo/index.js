833d082f3cbd34a71cb710292a12267306bf5dbe
Jason Gornall <jason.gornall@lessthan3.com>
2018-05-31 14:11:43 -0700

    tweaks

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
-
+import Maestro from './MaestroPlayer'
 const container = document.getElementById('app')
 
-render(<App />, container)
+//render(<App />, container)
+render(<Maestro />, container)
